779c6a461a6bf2cbe16131e5b199b40fbacf87f1
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-01-04 15:59:36 -0300

    feat: Add routes for Splash and Welcome modules

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,3 +1,21 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:poke_fusion/features/splash/splash_module.dart';
+import 'package:poke_fusion/features/welcome/welcome_module.dart';
+import 'package:poke_fusion/main_routes.dart';
 
-class AppModule extends Module {}
+class AppModule extends Module {
+  @override
+  void binds(i) {}
+
+  @override
+  void routes(r) {
+    r.module(
+      MainRoutes.splash.route,
+      module: SplashModule(),
+    );
+    r.module(
+      MainRoutes.welcome.route,
+      module: WelcomeModule(),
+    );
+  }
+}
