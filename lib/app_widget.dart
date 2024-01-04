0c8d08c7aaf82fd268ae791819ea366dd0de08ef
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-01-04 16:00:09 -0300

    Refactor: app_widget.dart to use Flutter Modular

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
-
-import 'features/splash/presenter/page/splash_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:poke_fusion/main_routes.dart';
 
 class PokeFusion extends StatelessWidget {
   const PokeFusion({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashPage(),
+    Modular.setInitialRoute(MainRoutes.splash.route);
+
+    return MaterialApp.router(
+      title: 'PokeFusion',
+      debugShowCheckedModeBanner: false,
+      routerConfig: Modular.routerConfig,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.red,
+      ),
+      builder: (context, child) {
+        return child!;
+      },
     );
   }
 }
