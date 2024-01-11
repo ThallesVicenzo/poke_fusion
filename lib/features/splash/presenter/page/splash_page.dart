a1ccb2482649f38f4d4558c721044a6ba0fce307
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-01-11 15:41:18 -0300

    Refactor SplashPage to use SplashController

--- lib/features/splash/presenter/page/splash_page.dart
+++ lib/features/splash/presenter/page/splash_page.dart
@@ -1,15 +1,16 @@
-import 'dart:async';
-
 import 'package:app_design/app_images.dart';
 import 'package:app_design/widgets/app_image_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../../../core/main_routes.dart';
+import '../controller/splash_controller.dart';
 
 class SplashPage extends StatefulWidget {
+  final SplashController controller;
   const SplashPage({
     super.key,
+    required this.controller,
   });
 
   @override
@@ -19,31 +20,31 @@ class SplashPage extends StatefulWidget {
 class _SplashScreen extends State<SplashPage> {
   @override
   void initState() {
-    Timer(const Duration(milliseconds: 1500), () {
-      setState(() {
-        opacity = 0;
-      });
-    });
+    widget.controller.getPokeData();
     super.initState();
   }
-
-  double opacity = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedOpacity(
-        opacity: opacity,
-        duration: kThemeAnimationDuration,
-        onEnd: () {
-          if (opacity == 0) {
-            Modular.to.navigate(MainRoutes.home.route);
-          }
-        },
-        child: const Center(
-          child: AppImageWidget(
-            image: AppImage.logo,
-            size: Size(300, 300),
+      body: ValueListenableBuilder(
+        valueListenable: widget.controller.opacity,
+        builder: (context, value, child) => AnimatedOpacity(
+          opacity: value,
+          duration: kThemeAnimationDuration,
+          onEnd: () {
+            if (value == 0) {
+              Modular.to.navigate(
+                MainRoutes.home.route,
+                arguments: widget.controller.state.value.asSuccess,
+              );
+            }
+          },
+          child: const Center(
+            child: AppImageWidget(
+              image: AppImage.logo,
+              size: Size(300, 300),
+            ),
           ),
         ),
       ),
