3840bc3b06cee93844d6b9d5eece6fae053d4d61
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-01-16 16:17:38 -0300

    Add fusion functionality to HomeController

--- lib/features/home/presenter/controller/home_controller.dart
+++ lib/features/home/presenter/controller/home_controller.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:poke_fusion/core/page_state.dart';
+import 'package:poke_fusion/features/home/domain/entities/fusion_entity.dart';
+import 'package:poke_fusion/features/home/domain/usecases/fusion_usecase.dart';
 
 class HomeController extends ChangeNotifier {
-  ValueNotifier<String> selectedPokemon = ValueNotifier('select a pokémon');
-  String? image;
+  final FusionUseCase useCase;
 
-  void updateSelectedPokemon(
-      String? selectedPokemonName, String? selectedPokemonImage) {
-    selectedPokemon.value = selectedPokemonName!;
-    notifyListeners();
+  HomeController({required this.useCase});
+
+  final state = ValueNotifier<PageState<FusionEntity>>(InitialState());
+
+  Future<void> getFusion({
+    required headPokemon,
+    required bodyPokemon,
+  }) async {
+    state.value = LoadingState();
+    final result = await useCase.call(headPokemon, bodyPokemon);
+
+    result.deal(
+      onFail: (e) => state.value = ErrorState(error: e),
+      onSuccess: (s) => state.value = SuccessState(data: s),
+    );
   }
 }
