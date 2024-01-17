b486578e2383eadb102649ade461cf5bfb6c2c25
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-01-17 14:37:24 -0300

    Add disable functionality to DefaultButton widget

--- app_design/lib/widgets/default_button.dart
+++ app_design/lib/widgets/default_button.dart
@@ -5,31 +5,36 @@ class DefaultButton extends StatelessWidget {
     super.key,
     required this.title,
     this.size,
+    this.desable = false,
     required this.onPressed,
   });
 
   final Size? size;
   final String title;
   final VoidCallback onPressed;
+  final bool desable;
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: onPressed,
-      child: Container(
-        constraints: BoxConstraints(
-          maxWidth: size?.width ?? MediaQuery.of(context).size.width * 0.4,
-          minHeight: size?.height ?? 32,
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: Colors.blueAccent,
-        ),
-        child: Center(
-          child: Text(
-            title,
-            style: const TextStyle(
-              color: Colors.white,
+    return AbsorbPointer(
+      absorbing: desable,
+      child: TextButton(
+        onPressed: onPressed,
+        child: Container(
+          constraints: BoxConstraints(
+            maxWidth: size?.width ?? MediaQuery.of(context).size.width * 0.4,
+            minHeight: size?.height ?? 32,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: desable ? Colors.grey : Colors.blueAccent,
+          ),
+          child: Center(
+            child: Text(
+              title,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
             ),
           ),
         ),
