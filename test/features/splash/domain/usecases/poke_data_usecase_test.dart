c787ae5775b5c0ecf56ad1398219b6e959a6d26e
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-01-19 14:33:24 -0300

    test: add PokeDataUseCaseImp unit tests

---
+++ test/features/splash/domain/usecases/poke_data_usecase_test.dart
@@ -0,0 +1,65 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:poke_fusion/core/domain/response_error.dart';
+import 'package:poke_fusion/core/response.dart';
+import 'package:poke_fusion/features/splash/domain/entities/poke_data_entity.dart';
+import 'package:poke_fusion/features/splash/domain/repository/poke_data_repository.dart';
+import 'package:poke_fusion/features/splash/domain/usecases/poke_data_usecase.dart'; // Importe a biblioteca Mocktail
+
+// Crie uma classe mock para o PokeDataRepository
+class MockPokeDataRepository extends Mock implements PokeDataRepository {}
+
+void main() {
+  group('PokeDataUseCaseImp', () {
+    late PokeDataUseCaseImp pokeDataUseCase;
+    late MockPokeDataRepository mockRepository;
+
+    setUp(() {
+      // Configurar o ambiente de teste
+      mockRepository = MockPokeDataRepository();
+      pokeDataUseCase = PokeDataUseCaseImp(repository: mockRepository);
+    });
+
+    List<PokeDataEntity> success = [
+      PokeDataEntity(
+        name: faker.animal.name(),
+      ),
+      PokeDataEntity(
+        name: faker.animal.name(),
+      ),
+    ];
+
+    test('should return a Response Success when repository is called',
+        () async {
+      // Arrange
+      when(() => mockRepository.call())
+          .thenAnswer((_) async => Response.success(success));
+
+      // Act
+      final result = await pokeDataUseCase();
+
+      expect(result.isSuccess, true);
+      expect(result.asSuccess, success);
+
+      // Assert
+      verify(() => mockRepository.call()).called(1);
+    });
+
+    test('should return a list of PokeDataEntity from repository', () async {
+      final tResponseFailure = ResponseGenericError(error: Exception());
+
+      // Arrange
+      when(() => mockRepository.call()).thenAnswer(
+        (i) async => Response.fail(tResponseFailure),
+      );
+
+      // Act
+      final result = await pokeDataUseCase();
+
+      // Assert
+      expect(result.isFailure, isTrue);
+      expect(result.asFailure, tResponseFailure);
+    });
+  });
+}
