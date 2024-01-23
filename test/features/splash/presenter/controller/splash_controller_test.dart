fec478eb4231b1c85e30060901246979a37a98b3
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-01-23 14:31:57 -0300

    test: adds Splash controller test

---
+++ test/features/splash/presenter/controller/splash_controller_test.dart
@@ -0,0 +1,67 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:poke_fusion/core/domain/response_error.dart';
+import 'package:poke_fusion/core/response.dart';
+import 'package:poke_fusion/features/splash/domain/entities/poke_data_entity.dart';
+import 'package:poke_fusion/features/splash/domain/usecases/poke_data_usecase.dart';
+import 'package:poke_fusion/features/splash/presenter/controller/splash_controller.dart';
+
+class MockPokeDataUseCase extends Mock implements PokeDataUseCase {}
+
+void main() {
+  group('Splash Controller', () {
+    late SplashController sut;
+    late MockPokeDataUseCase mockPokeDataUseCase;
+
+    setUp(() {
+      mockPokeDataUseCase = MockPokeDataUseCase();
+      sut = SplashController(useCase: mockPokeDataUseCase);
+    });
+
+    group('get poke data', () {
+      test('should handle fail correctly', () async {
+        final error = ResponseGenericError(
+          error: 'Error',
+          errorCode: 500,
+        );
+
+        when(() => mockPokeDataUseCase.call()).thenAnswer(
+          (_) async => Response.fail(error),
+        );
+
+        await sut.getPokeData();
+
+        expect(
+          sut.state.value.asError,
+          isA<ResponseGenericError>(),
+        );
+
+        verify(() => mockPokeDataUseCase.call()).called(1);
+      });
+
+      test('Should handle success correctly', () async {
+        final fakeList = [
+          PokeDataEntity(name: faker.animal.name()),
+          PokeDataEntity(name: faker.animal.name()),
+          PokeDataEntity(name: faker.animal.name()),
+        ];
+
+        when(() => mockPokeDataUseCase.call()).thenAnswer(
+          (_) async => Response.success(fakeList),
+        );
+
+        await sut.getPokeData();
+
+        expect(sut.opacity.value, equals(0));
+
+        expect(
+          sut.state.value.asSuccess,
+          equals(fakeList),
+        );
+
+        verify(() => mockPokeDataUseCase.call()).called(1);
+      });
+    });
+  });
+}
