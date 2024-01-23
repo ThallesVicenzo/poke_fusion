4a5478ce8f783a42e07d9e1408ab9c2bcfaa6ebf
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-01-23 10:17:04 -0300

    test: add test to PokeDataDataSourceImpl

---
+++ test/features/splash/external/pokedata_data_source_imp_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:faker/faker.dart';
+import 'package:poke_fusion/core/client/client_http.dart';
+import 'package:poke_fusion/core/client/client_http_response.dart';
+import 'package:poke_fusion/features/splash/external/pokedata_data_source_imp.dart';
+
+// Crie uma classe mock para o Dio
+class MockDio extends Mock implements ClientHttp {}
+
+void main() {
+  group('PokeDataDataSourceImpl', () {
+    late PokeDataDataSourceImpl pokeDataDataSource;
+    late MockDio mockClientHttp;
+    late Faker faker;
+
+    setUp(() {
+      // Configurar o ambiente de teste
+      mockClientHttp = MockDio();
+      pokeDataDataSource = PokeDataDataSourceImpl(
+        clientHttp: mockClientHttp,
+      );
+      faker = Faker();
+    });
+
+    test('should return a PokeDataModel when request is made', () async {
+      // Arrange
+      final fakePokemonData = [
+        {'name': faker.animal.name()},
+        {'name': faker.animal.name()},
+      ];
+
+      final responseData = {'pokemon_species': fakePokemonData};
+
+      when(() => mockClientHttp.get(any())).thenAnswer(
+        (_) async => ClientHttpResponse(
+          data: responseData,
+        ),
+      );
+
+      // Act
+      final result = await pokeDataDataSource();
+
+      // Assert
+      expect(result.length, equals(fakePokemonData.length));
+      expect(result[0].name, equals(fakePokemonData[0]['name']));
+      expect(result[1].name, equals(fakePokemonData[1]['name']));
+      // Add more assertions as needed
+    });
+  });
+}
